75671b12f52e112df594a2ee677ae59b025c0148
Juan Nunez <Juan.Nunez612@gmail.com>
2021-01-29 16:34:27 -0600

    added the variables

--- dom/shopping/shoppingList.js
+++ dom/shopping/shoppingList.js
@@ -1,41 +1,46 @@
-/*
-    Do the following steps in this file, and make sure this file is linked to the shopping-list.html file
 
-    
+    // Do the following steps in this file, and make sure this file is linked to the shopping-list.html file
 
-    1. Create three variables that hold references to the following elements &lt;ul&gt; &lt;input&gt; &lt;button&gt; elements.
+    //done
+
+    // 1. Create three variables that hold references to the following elements &lt;ul&gt; &lt;input&gt; &lt;button&gt; elements.
     
+    const ul = document.createElement('ul')
+    const input = document.createElement('input')
+    const button = document.createElement('button')
+
+    //done
     
-    2. Create a function that will run in response to the button being clicked.
-    
-    
-    
-    3. Inside the function body, start off by storing the current value of the input element in a variable.
-    
-    
-    
-    4. Next, empty the input element by setting its value to an empty string — ''.
-    
-    
-    
-    5. Create three new elements — a list item (&lt;li&gt;), &lt;span&gt;, and &lt;button&gt;, and store them in variables.
+    // 2. Create a function that will run in response to the button being clicked.
     
     
     
-    6. Append the span and the button as children of the list item.
+    // 3. Inside the function body, start off by storing the current value of the input element in a variable.
     
     
     
-    7. Set the text content of the span to the input element value you saved earlier, and the text content of the button to 'Delete'.
+    // 4. Next, empty the input element by setting its value to an empty string — ''.
     
     
     
-    8. Append the list item as a child of the list.
+    // 5. Create three new elements — a list item (&lt;li&gt;), &lt;span&gt;, and &lt;button&gt;, and store them in variables.
     
     
     
-    9. Attach an event handler to the delete button, so that when clicked it will delete the entire list item it is inside.
+    // 6. Append the span and the button as children of the list item.
     
     
     
-    10. Finally, use the focus() method to focus the input element ready for entering the next shopping list item.
+    // 7. Set the text content of the span to the input element value you saved earlier, and the text content of the button to 'Delete'.
+    
+    
+    
+    // 8. Append the list item as a child of the list.
+    
+    
+    
+    // 9. Attach an event handler to the delete button, so that when clicked it will delete the entire list item it is inside.
+    
+    
+    
+    // 10. Finally, use the focus() method to focus the input element ready for entering the next shopping list item.
