d3827f4e057a84b773cdb4c68d3ed5a5666c1f36
Juan Nunez <Juan.Nunez612@gmail.com>
2021-01-29 02:49:01 -0600

    worked on the dom

--- dom/inject/inject.js
+++ dom/inject/inject.js
@@ -1,4 +1,12 @@
 /*Exercise instructions : take the html code from the following codepen : https://codepen.io/ayunas-the-scripter/pen/WNQgpqB, and use JavaScript DOM manipulation to inject the html into the inject id element of inject.html.  You are not allowed to simply set the innerHTML of the <div> to the entire html document. 
 */
 
+const inject = document.getElementById('inject')
+
+const centerTag = document.createElement('center')
+// inject.appendChild(centerTag)
+console.log("hi")
+var italizeTag = document.createElement('i')
+italizeTag.innerHTML = 'HTML Practice Exercise TEKcamp.'
+
 
