a2bbc5f9f9725af16cd2b640ef3dc62fe1030ced
Juan Nunez <Juan.Nunez612@gmail.com>
2021-01-30 23:20:51 -0600

    added reference to the ids

--- dom/timer/timer-dom.js
+++ dom/timer/timer-dom.js
@@ -1,1 +1,16 @@
 console.log("time to start the timer!");
+
+function stopwatch (x,y){
+
+const start = document.getElementById('start')
+const stop = document.getElementById('stop')
+const clock = document.getElementById('clock')
+const reset = document.getElementById('reset')
+
+
+
+
+
+
+
+}
