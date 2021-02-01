938a7ea4335cdbee47970f2ae278fb49d58f99d0
Juan Nunez <Juan.Nunez612@gmail.com>
2021-02-01 11:08:36 -0600

    added semicolons and finished exercise

--- dom/shopping/shoppingList.js
+++ dom/shopping/shoppingList.js
@@ -4,9 +4,9 @@
     //done
     // 1. Create three variables that hold references to the following elements &lt;ul&gt; &lt;input&gt; &lt;button&gt; elements.
     
-    const unorderedList = document.querySelector('ul')
-    const input = document.querySelector('input')
-    const buttonOne = document.querySelector('button')
+    const unorderedList = document.querySelector('ul');
+    const input = document.querySelector('input');
+    const buttonOne = document.querySelector('button');
 
     //done
     
@@ -17,47 +17,47 @@
     // 4. Next, empty the input element by setting its value to an empty string — ''.
     //done
     buttonOne.onclick = function(){
-        let userInputs = input.value
-        input.value = ''
+        let userInputs = input.value;
+        input.value = '';
     
     
     //done
     // 5. Create three new elements — a list item (&lt;li&gt;), &lt;span&gt;, and &lt;button&gt;, and store them in variables.
     
 
-    const listItem = document.createElement('li')
-    const span = document.createElement('span')
-    const buttonTwo = document.createElement('button')
+    const listItem = document.createElement('li');
+    const span = document.createElement('span');
+    const buttonTwo = document.createElement('button');
     
     //done
     
     // 6. Append the span and the button as children of the list item.
     
-    listItem.appendChild(span)
-    listItem.appendChild(buttonTwo)
+    listItem.appendChild(span);
+    listItem.appendChild(buttonTwo);
     
     //done
 
     // 7. Set the text content of the span to the input element value you saved earlier, and the text content of the button to 'Delete'.
     
-    span.textContent = userInputs
-    buttonTwo.textContent = 'Delete'
+    span.textContent = userInputs;
+    buttonTwo.textContent = 'Delete';
     
     //done
 
     // 8. Append the list item as a child of the list.
-    unorderedList.appendChild(listItem)
+    unorderedList.appendChild(listItem);
     
     //done 
 
     // 9. Attach an event handler to the delete button, so that when clicked it will delete the entire list item it is inside.
     
     buttonTwo.onclick = function(){
-        unorderedList.removeChild(listItem)
+        unorderedList.removeChild(listItem);
     }
     
     // 10. Finally, use the focus() method to focus the input element ready for entering the next shopping list item.
 
-    input.focus()
+    input.focus();
 
 }
