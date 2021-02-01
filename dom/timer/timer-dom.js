34d6bcb96b36b2874cae14d60e522f54f677fdfc
Juan Nunez <Juan.Nunez612@gmail.com>
2021-01-31 22:40:56 -0600

    added time conversion function

--- dom/timer/timer-dom.js
+++ dom/timer/timer-dom.js
@@ -1,11 +1,25 @@
 console.log("time to start the timer!");
 
-function stopwatch (x,y){
-
 const start = document.getElementById('start')
 const stop = document.getElementById('stop')
 const clock = document.getElementById('clock')
 const reset = document.getElementById('reset')
+const clock = document.getElementById('clock')
+let hrs = 0
+let secs = 0
+let mins =0
+
+function timeConversion(){
+    secs++
+    if (secs >= 60){
+        secs = 0
+        mins++
+        if (mins >= 60){
+            minutes = 0
+            hours++
+        }
+    }
+}
 
 
 
